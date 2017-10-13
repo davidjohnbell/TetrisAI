80d18124d08de3426ea884b119435c02a46c85c5
david <david_bell@live.ca>
2017-10-13 02:33:54 -0600

    Changing board to extend Matrix - rewrote tests

--- src/tests/BoardTest.java
+++ src/tests/BoardTest.java
@@ -40,41 +40,50 @@ class BoardTest {
                 {0,0,0,0},
                 {0,0,0,0},
                 {0,0,0,0}});
-        Matrix is = Board.applyShape(shape, board.board);
+        Matrix is = board.applyShape(shape);
         Assertions.assertTrue(is.equals(shouldBe));
     }
 
     @Test
     void collision() {
-        board.board.setElement(1,0, 5);
-        Matrix applied = Board.applyShape(shape, board.board);
-        boolean oops = Board.collision(applied, board.board);
+        board.setElement(1,0, 5);
+        Matrix applied = board.applyShape(shape);
+        boolean oops = board.collision(applied);
         Assertions.assertTrue(oops);
     }
 
     @Test
-    void rowsCleared() {
-        for(int i = 0; i < board.board.getHeight(); i += 2) {
-            for(int j = 0; j < board.board.getWidth(); j++) {
-                board.board.setElement(i, j, 1);
+    void getFullRows() {
+        for(int i = 0; i < board.getHeight(); i += 2) {
+            for(int j = 0; j < board.getWidth(); j++) {
+                board.setElement(i, j, 1);
             }
         }
-        for(int i = 0; i < board.board.getHeight(); i ++) {
-            board.board.setElement(i, 0, 1);
-        }
-        int[] cleared = Board.getFullRows(board.board);
-        Assertions.assertArrayEquals(cleared,
-                new int[] {1,0,1,0,1,0,1,0});
-        Assertions.assertEquals(4, board.board.sumCol(0));
-        Assertions.assertEquals(0, board.board.sumCol(1));
+        int[] cleared = board.getFullRows();
+        Assertions.assertArrayEquals(cleared, new int[] {1,0,1,0,1,0,1,0});
     }
 
     @Test
-    void win() {
-        board.board.setElement(0, 0, 1);
-        boolean win = Board.isGameOver(board.board);
-        //Unimplemented
-        Assertions.assertTrue(win);
+    void collapseRows() {
+        for(int i = 0; i < board.getHeight(); i += 2) {
+            for(int j = 0; j < board.getWidth(); j++) {
+                board.setElement(i, j, 1);
+            }
+        }
+        for(int i = 0; i < board.getHeight(); i ++) {
+            board.setElement(i, 0, 1);
+        }
+        int[] cleared = board.getFullRows();
+        Board collapsed = board.collapseRows(cleared);
+        int sum = collapsed.sumRow(0);
+        Assertions.assertEquals(4,sum);
+    }
+
+    @Test
+    void isGameOver() {
+        board.setElement(0, 0, 1);
+        boolean lost = board.isGameOver();
+        Assertions.assertTrue(lost);
     }
 
 }
